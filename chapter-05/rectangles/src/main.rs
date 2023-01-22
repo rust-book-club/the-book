2c32bd704df62a376d858c1593bff0ec227bdeaa
Andrew William Watson <aww@awwsmm.com>
2023-01-22 10:08:58 -0500

    Ch05 rectangles the trait  is not implemented for

--- chapter-05/rectangles/src/main.rs
+++ chapter-05/rectangles/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
         height: 50
     };
 
-    println!("rect1 is {}", rect1);
+    println!("rect1 is {:?}", rect1);
 
     println!(
         "The area of the rectangle is {} square pixels.",
