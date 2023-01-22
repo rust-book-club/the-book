032d1daaab467eb85cc9d8c47eb0ce9389a9302e
Andrew William Watson <aww@awwsmm.com>
2023-01-22 09:48:18 -0500

    Ch05 rectangles tuple

--- chapter-05/rectangles/src/main.rs
+++ chapter-05/rectangles/src/main.rs
@@ -1,13 +1,12 @@
 fn main() {
-    let width1 = 30;
-    let height1 = 50;
+    let rect1 = (30, 50);
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(width1, height1)
+        area(rect1)
     )
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
