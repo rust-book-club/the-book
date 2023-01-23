c0bf4e330b85dec9022749a11e1a3153985f0e08
Andrew William Watson <aww@awwsmm.com>
2023-01-22 21:02:31 -0500

    Ch06 even more IpAddr improvements

--- chapter-06/ipaddr/src/main.rs
+++ chapter-06/ipaddr/src/main.rs
@@ -1,5 +1,5 @@
 enum IpAddr {
-    V4(String),
+    V4(u8, u8, u8, u8),
     V6(String)
 }
 
@@ -7,7 +7,7 @@ fn main() {
     let four = IpAddr::V4;
     let six = IpAddr::V6;
 
-    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let home = IpAddr::V4(127, 0, 0, 1);
 
     let loopback = IpAddr::V6(String::from("::1"));
 
